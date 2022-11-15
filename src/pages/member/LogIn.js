0e0e8cf1ea5b1a6cbe0600a948e0af5b87d62ab0
Chloe <chloe012125@gmail.com>
2022-11-15 18:59:28 +0800

    登入表單樣式

--- src/pages/member/LogIn.js
+++ src/pages/member/LogIn.js
@@ -4,24 +4,22 @@ import '../../global.scss';
 import Logo from '../../icon/logo/logo_white.svg';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
 
 function logIn() {
   return (
     <>
-      <div className="container">
+      <div className="container ">
         <div className="row m-auto">
           <div className="m-flex">
-            <div className="logo m-auto col-6">
+            <div className="logo m-auto">
               <img src={Logo} alt="logo" />
             </div>
-            <Form className="form col-6 m-auto padding-form">
+            <Form className="form col-5 m-auto">
               <h1 className="login-text text-center pb-5">登入</h1>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email</Form.Label>
                 <Form.Control type="email" placeholder="example@mail.com" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -30,11 +28,14 @@ function logIn() {
                   type="password"
                   placeholder="8位以上英數密碼，請區分大小寫"
                 />
+                <Link className="forget-password text-end">忘記密碼？</Link>
               </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
-              <Button className="mx-auto" variant="primary" type="submit">
+
+              <Button
+                className="login-button d-flex"
+                variant="primary"
+                type="submit"
+              >
                 登入
               </Button>
             </Form>
