f2807904bcb46f31a66dc58b27dc5ac7229dfe3a
Jie-Mac <e25254@gmail.com>
2022-11-16 11:28:14 +0800

    fix

--- src/pages/product/stays/ComputerLikeAdd/ComputerLikeAdd.js
+++ src/pages/product/stays/ComputerLikeAdd/ComputerLikeAdd.js
@@ -4,20 +4,21 @@ import { FaCalendarAlt } from 'react-icons/fa';
 import './ComputerLikeAdd.scss';
 
 function ComputerLikeAdd() {
-	return (
-		<>
-			<div
-				className="LikeAddOnCom"
-				onClick={() => {}}
-				style={{
-					width: '40px',
-					height: '40px',
-					padding: '8px',
-					// marginRight: '20px',
-				}}
-			>
-				{/* TODO:點下去變色 加入收藏 */}
-				{/* <BsHeart
+  return (
+    <>
+      <div
+        className="LikeAddOnCom"
+        onClick={() => {}}
+        style={{
+          width: '40px',
+          height: '40px',
+          padding: '8px',
+          zIndex: '-1',
+          // marginRight: '20px',
+        }}
+      >
+        {/* TODO:點下去變色 加入收藏 */}
+        {/* <BsHeart
 						style={{
 							width: '100%',
 							height: '100%',
@@ -25,30 +26,32 @@ function ComputerLikeAdd() {
 							color: '#aeaeae',
 						}}
 					/> */}
-				<BsHeartFill
-					style={{
-						width: '100%',
-						height: '100%',
-						transform: 'translateY(2px) translateX(5px)',
-						color: '#EA8A8A',
-					}}
-				/>
-			</div>
-			<div
-				onClick={() => {}}
-				style={{
-					width: '40px',
-					height: '40px',
-					padding: '8px',
-					marginRight: '5px',
-				}}
-			>
-				{/* TODO:點下去變色 加入行成 */}
-				{/* <FaCalendarAlt style={{ width: '100%', height: '100%', color: '#aeaeae' }} /> */}
-				<FaCalendarAlt style={{ width: '100%', height: '100%', color: '#59D8A1' }} />
-			</div>
-		</>
-	);
+        <BsHeartFill
+          style={{
+            width: '100%',
+            height: '100%',
+            transform: 'translateY(2px) translateX(5px)',
+            color: '#EA8A8A',
+          }}
+        />
+      </div>
+      <div
+        onClick={() => {}}
+        style={{
+          width: '40px',
+          height: '40px',
+          padding: '8px',
+          marginRight: '5px',
+        }}
+      >
+        {/* TODO:點下去變色 加入行成 */}
+        {/* <FaCalendarAlt style={{ width: '100%', height: '100%', color: '#aeaeae' }} /> */}
+        <FaCalendarAlt
+          style={{ width: '100%', height: '100%', color: '#59D8A1' }}
+        />
+      </div>
+    </>
+  );
 }
 
 export default ComputerLikeAdd;
