5699b659c416ce17863e50bd1b6e2a73871bcd86
Chloe <chloe012125@gmail.com>
2022-11-17 10:41:02 +0800

    登入表單前端設定

--- src/pages/member/LogIn.js
+++ src/pages/member/LogIn.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { useContext, useState } from 'react';
+import axios from 'axios';
+import { LOGIN_API } from '../my-config';
+import AuthContext from './context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 import '../member/style/LogIn.scss';
 import '../../global.scss';
 import Logo from '../../icon/logo/logo_white.svg';
@@ -6,7 +11,37 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Link } from 'react-router-dom';
 
-function logIn() {
+function LogIn() {
+  const { setMyAuth } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    account: '',
+    password: '',
+  });
+
+  const handler = (e) => {
+    const id = e.currentTarget.id;
+    const val = e.currentTarget.value;
+    // console.log({id, val});
+
+    setFormData({ ...formData, [id]: val });
+  };
+
+  const mySubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await axios.post(LOGIN_API, formData);
+    console.log(data);
+    if (data.success) {
+      localStorage.setItem('auth', JSON.stringify(data.auth));
+      alert('登入成功');
+      console.log(JSON.stringify(data));
+      setMyAuth({ ...data.auth, authorised: true });
+      navigate('/');
+    } else {
+      localStorage.removeItem('auth'); // 移除
+      alert('登入失敗');
+    }
+  };
   return (
     <>
       <div className="container ">
@@ -16,11 +51,17 @@ function logIn() {
               <img src={Logo} alt="logo" />
             </Link>
 
-            <Form className="form col-5 m-auto">
+            <Form className="form col-5 m-auto" onSubmit={mySubmit}>
               <h1 className="login-text text-center pb-5">登入</h1>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="example@mail.com" />
+                <Form.Control
+                  type="email"
+                  placeholder="example@mail.com"
+                  id="account"
+                  onChange={handler}
+                  value={formData.account}
+                />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -28,6 +69,9 @@ function logIn() {
                 <Form.Control
                   type="password"
                   placeholder="8位以上英數密碼，請區分大小寫"
+                  id="password"
+                  onChange={handler}
+                  value={formData.password}
                 />
                 <Link className="forget-password-text" to="/forget_password">
                   忘記密碼？
@@ -55,4 +99,4 @@ function logIn() {
   );
 }
 
-export default logIn;
+export default LogIn;
