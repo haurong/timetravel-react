f2d8823f37f1ae06373d4f1891b807457684c1dc
Chloe <chloe012125@gmail.com>
2022-11-20 16:02:45 +0800

    編輯頁面大頭樣式設定

--- src/pages/member/Profile.js
+++ src/pages/member/Profile.js
@@ -9,9 +9,54 @@ function Profile() {
   return (
     <>
       <NavBar />
-      <div className="container ">
-        <div className="givePadding profile_padding">
+      <div className="container">
+        <div className="givePadding profile_padding d-flex">
           <SideBar />
+          <div className="profile col-5">
+            <h1>修改個人資訊</h1>
+            <div className="profile_img">
+              <div class="profile-hovor">
+                <div class="profile-hovor-text text-center">編輯</div>
+              </div>
+            </div>
+            {/* <img className="profile_img" src="" /> */}
+
+            <form className="form">
+              <div className="mb-3 profile-input">
+                <label htmlFor="inputName" className="form-label ">
+                  姓名
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="inputName"
+                  placeholder="王小明"
+                />
+              </div>
+              <div className="mb-3 profile-input">
+                <label htmlFor="inputEmail4" className="form-label">
+                  手機號碼
+                </label>
+                <input
+                  type="tel"
+                  className="form-control"
+                  id="phone"
+                  name="phone"
+                  pattern="[0-9]{4}-[0-9]{3}-[0-9]{3}"
+                  required
+                  placeholder="09xx-xxx-xxx"
+                />
+              </div>
+              <div className="mb-3 mx-auto">
+                <button
+                  type="submit"
+                  className="btn btn-primary profile-button d-flex mx-auto"
+                >
+                  儲存
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
       <Footer />
