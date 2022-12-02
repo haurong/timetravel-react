2db7ab771eda68a9d7da0bb182d9194a3ca00687
Liufat <dd5672003@gmail.com>
2022-12-02 14:20:51 +0800

    修改樣式

--- src/pages/cart/order/components/OrdersAccordion.js
+++ src/pages/cart/order/components/OrdersAccordion.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import SortIcon from './../../../../icon/sort.svg';
 import axios from 'axios';
+import Accordion from 'react-bootstrap/Accordion';
 import { ORDER_DETAILS_FOOD_API } from '../../../../config';
 function OrdersAccordion({ createdTime, uuid, totalPrice }) {
-  const [show, setShow] = useState(false);
   const [foodOrdersData, setFoodOrdersData] = useState([]);
   async function getFoodOrders() {
     const response = await axios.get(ORDER_DETAILS_FOOD_API(uuid));
@@ -14,106 +14,82 @@ function OrdersAccordion({ createdTime, uuid, totalPrice }) {
   }, [uuid]);
   // console.log(foodOrdersData);
   return (
-    <div className="accordion">
-      <div className="accordion-item">
-        <h2 className="accordion-header" id="headingOne">
-          <button
-            className="accordion-button"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapseOne"
-            aria-expanded="true"
-            aria-controls="collapseOne"
-            onClick={() => {
-              setShow(!show);
-              // console.log(show);
-            }}
-          >
-            <img alt="sort-img" className="sort-img" src={SortIcon} />
-            <div className="w-100 m-0 ">
-              <ul className="orders-accordion-ul p-0 m-0">
-                <li className="col-lg-4 text-center">
-                  <p>{createdTime}</p>
-                </li>
-                <li className="col-lg-4 text-center">
-                  <p>{uuid}</p>
-                </li>
-                <li className="col-lg-4 text-center">
-                  <p>{`TWD$${totalPrice}`}</p>
-                </li>
-              </ul>
-            </div>
-          </button>
-        </h2>
-        <div
-          id="collapseOne"
-          className={
-            show
-              ? 'accordion-collapse collapse show '
-              : 'accordion-collapse collapse '
-          }
-          aria-labelledby="headingOne"
-          data-bs-parent="#accordionExample"
-        >
-          <div className="accordion-body px-0">
-            <div className="w-100 m-0 ">
-              <ul className="orders-accordion-ul p-0 m-0 pb-2">
-                <li className="col text-center">
-                  <p>商品名稱</p>
-                </li>
-                <li className="col text-center">
-                  <p>商品單價</p>
-                </li>
-                <li className="col text-center">
-                  <p>商品數量</p>
-                </li>
-                <li className="col text-center">
-                  <p>小計</p>
-                </li>
-                <li className="col text-center">
-                  <p>評價</p>
-                </li>
-              </ul>
-              {foodOrdersData.map((v, i) => {
-                return (
-                  <div key={i}>
-                    <ul className="orders-accordion-ul p-0 m-0 pb-2 d-flex align-items-center">
-                      <li className="col text-center">
-                        <p>{v.product_name}</p>
-                      </li>
-                      <li className="col text-center">
-                        <p>{v.p_selling_price}</p>
-                      </li>
-                      <li className="col text-center">
-                        <p>{v.quantity}</p>
-                      </li>
-                      <li className="col text-center">
-                        <p>{`TWD$${v.total_price}`}</p>
-                      </li>
-                      <li className="col text-center">
-                        {v.committed === 1 ? (
-                          <button type="button" className="btn btn-primary">
-                            留下評價
-                          </button>
-                        ) : (
-                          <button
-                            type="button"
-                            className="btn btn-primary"
-                            disabled
-                          >
-                            已經評價
-                          </button>
-                        )}
-                      </li>
-                    </ul>
-                  </div>
-                );
-              })}
-            </div>
+    <Accordion defaultActiveKey="0">
+      <Accordion.Item>
+        <Accordion.Header>
+          <img alt="sort-img" className="sort-img" src={SortIcon} />
+          <div className="w-100 m-0 ">
+            <ul className="orders-accordion-ul p-0 m-0">
+              <li className="col-lg-4 text-center">
+                <p>{createdTime}</p>
+              </li>
+              <li className="col-lg-4 text-center">
+                <p>{uuid}</p>
+              </li>
+              <li className="col-lg-4 text-center">
+                <p>{`TWD$${totalPrice}`}</p>
+              </li>
+            </ul>
           </div>
-        </div>
-      </div>
-    </div>
+        </Accordion.Header>
+        <Accordion.Body>
+          <div className="w-100 m-0 ">
+            <ul className="orders-accordion-ul p-0 m-0 pb-2">
+              <li className="col text-center">
+                <p>商品名稱</p>
+              </li>
+              <li className="col text-center">
+                <p>商品單價</p>
+              </li>
+              <li className="col text-center">
+                <p>商品數量</p>
+              </li>
+              <li className="col text-center">
+                <p>小計</p>
+              </li>
+              <li className="col text-center">
+                <p>評價</p>
+              </li>
+            </ul>
+            {foodOrdersData.map((v, i) => {
+              return (
+                <div key={i}>
+                  <ul className="orders-accordion-ul p-0 m-0 pb-2 d-flex align-items-center">
+                    <li className="col text-center">
+                      <p>{v.product_name}</p>
+                    </li>
+                    <li className="col text-center">
+                      <p>{v.p_selling_price}</p>
+                    </li>
+                    <li className="col text-center">
+                      <p>{v.quantity}</p>
+                    </li>
+                    <li className="col text-center">
+                      <p>{`TWD$${v.total_price}`}</p>
+                    </li>
+                    <li className="col text-center">
+                      {v.committed === 1 ? (
+                        <button type="button" className="btn btn-primary">
+                          留下評價
+                        </button>
+                      ) : (
+                        <button
+                          type="button"
+                          className="btn btn-primary"
+                          disabled
+                        >
+                          已經評價
+                        </button>
+                      )}
+                    </li>
+                  </ul>
+                </div>
+              );
+            })}
+          </div>
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
   );
 }
 
--- src/pages/cart/order/components/OrdersAccordionUndone.js
+++ src/pages/cart/order/components/OrdersAccordionUndone.js
@@ -1,103 +1,79 @@
-import React, { useState } from 'react';
+import React from 'react';
 import SortIcon from './../../../../icon/sort.svg';
+import Accordion from 'react-bootstrap/Accordion';
 function OrdersAccordionUndone() {
-  const [show, setShow] = useState(false);
   return (
-    <div className="accordion">
-      <div className="accordion-item">
-        <h2 className="accordion-header" id="headingOne">
-          <button
-            className="accordion-button"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapseOne"
-            aria-expanded="true"
-            aria-controls="collapseOne"
-            onClick={() => {
-              setShow(!show);
-              // console.log(show);
-            }}
-          >
-            <img alt="sort-img" className="sort-img" src={SortIcon} />
-            <div className="w-100 m-0 ">
-              <ul className="orders-accordion-ul p-0 m-0">
-                <li className="col-lg-3 text-center">
-                  <p>2022/10/18</p>
-                </li>
-                <li className="col-lg-3 text-center">
-                  <p>12938909049</p>
-                </li>
-                <li className="col-lg-3 text-center">
-                  <p>TWD$9831</p>
-                </li>
-                <li className="col-lg-3 text-center">
-                  <button type="button" className="btn btn-danger">
-                    尚未付款
-                  </button>
-                </li>
-              </ul>
-            </div>
-          </button>
-        </h2>
-        <div
-          id="collapseOne"
-          className={
-            show
-              ? 'accordion-collapse collapse show '
-              : 'accordion-collapse collapse '
-          }
-          aria-labelledby="headingOne"
-          data-bs-parent="#accordionExample"
-        >
-          <div className="accordion-body px-0">
-            <div className="w-100 m-0 ">
-              <ul className="orders-accordion-ul p-0 m-0 pb-2">
-                <li className="col text-center">
-                  <p>商品名稱</p>
-                </li>
-                <li className="col text-center">
-                  <p>商品單價</p>
-                </li>
-                <li className="col text-center">
-                  <p>商品數量</p>
-                </li>
-                <li className="col text-center">
-                  <p>小計</p>
-                </li>
-              </ul>
-              <ul className="orders-accordion-ul p-0 m-0 pb-2 d-flex align-items-center">
-                <li className="col text-center">
-                  <p>JR東日本大飯店</p>
-                </li>
-                <li className="col text-center">
-                  <p>TWD$599</p>
-                </li>
-                <li className="col text-center">
-                  <p>1</p>
-                </li>
-                <li className="col text-center">
-                  <p>TWD$599</p>
-                </li>
-              </ul>
-              <ul className="orders-accordion-ul p-0 m-0 pb-2 d-flex align-items-center">
-                <li className="col text-center">
-                  <p>JR東日本大飯店</p>
-                </li>
-                <li className="col text-center">
-                  <p>TWD$599</p>
-                </li>
-                <li className="col text-center">
-                  <p>1</p>
-                </li>
-                <li className="col text-center">
-                  <p>TWD$599</p>
-                </li>
-              </ul>
-            </div>
+    <Accordion>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>
+          <img alt="sort-img" className="sort-img" src={SortIcon} />
+          <div className="w-100 m-0 ">
+            <ul className="orders-accordion-ul p-0 m-0">
+              <li className="col-lg-3 text-center">
+                <p>2022/10/18</p>
+              </li>
+              <li className="col-lg-3 text-center">
+                <p>12938909049</p>
+              </li>
+              <li className="col-lg-3 text-center">
+                <p>TWD$9831</p>
+              </li>
+              <li className="col-lg-3 text-center">
+                <button type="button" className="btn btn-danger">
+                  尚未付款
+                </button>
+              </li>
+            </ul>
           </div>
-        </div>
-      </div>
-    </div>
+        </Accordion.Header>
+        <Accordion.Body>
+          <div className="w-100 m-0 ">
+            <ul className="orders-accordion-ul p-0 m-0 pb-2">
+              <li className="col text-center">
+                <p>商品名稱</p>
+              </li>
+              <li className="col text-center">
+                <p>商品單價</p>
+              </li>
+              <li className="col text-center">
+                <p>商品數量</p>
+              </li>
+              <li className="col text-center">
+                <p>小計</p>
+              </li>
+            </ul>
+            <ul className="orders-accordion-ul p-0 m-0 pb-2 d-flex align-items-center">
+              <li className="col text-center">
+                <p>JR東日本大飯店</p>
+              </li>
+              <li className="col text-center">
+                <p>TWD$599</p>
+              </li>
+              <li className="col text-center">
+                <p>1</p>
+              </li>
+              <li className="col text-center">
+                <p>TWD$599</p>
+              </li>
+            </ul>
+            <ul className="orders-accordion-ul p-0 m-0 pb-2 d-flex align-items-center">
+              <li className="col text-center">
+                <p>JR東日本大飯店</p>
+              </li>
+              <li className="col text-center">
+                <p>TWD$599</p>
+              </li>
+              <li className="col text-center">
+                <p>1</p>
+              </li>
+              <li className="col text-center">
+                <p>TWD$599</p>
+              </li>
+            </ul>
+          </div>
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
   );
 }
 
