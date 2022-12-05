18d859f243c1f79522c21004486ff4293399d376
Liufat <dd5672003@gmail.com>
2022-12-06 00:32:57 +0800

    結帳成功/失敗頁面

--- src/pages/cart/OrderFail.js
+++ src/pages/cart/OrderFail.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import NavBar from '../../layout/NavBar';
 import Footer from '../../layout/Footer';
+import FailIcon from './../../icon/delete_cart.svg';
+import { NavLink } from 'react-router-dom';
 function OrderFail() {
   return (
     <>
       <NavBar />
-      <div className="givePadding profile_padding">OrderFail</div>;
+      <div className="container success-fail">
+        <div className="space"></div>
+        <div className="img-wrap">
+          <img className="success-fail-icon" src={FailIcon} alt="SuccessIcon" />
+        </div>
+        <h2 className="fail">付款失敗</h2>
+        <p>喔喔！付款失敗</p>
+        <br />
+        <p className="pb-5">
+          但不用擔心，您可以前往
+          <NavLink to={'/orders'}>會員/訂單紀錄/交易中訂單</NavLink>
+          ，嘗試重新付款
+        </p>
+      </div>
+
       <Footer />
     </>
   );
